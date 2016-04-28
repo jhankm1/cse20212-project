e42caa08c3c09aae06ceb65ae1f7feccf8533fda
megob33 <mobryan@nd.edu>
2016-04-27 23:46:58 -0400

    Update game.h

--- game.h
+++ game.h
@@ -1,5 +1,5 @@
-#ifndef TEST_H
-#define TEST_H
+#ifndef GAME_H
+#define GAME_H
 
 //The headers
 #include "SDL/SDL.h"
@@ -8,27 +8,31 @@
 #include <string>
 #include <iostream>
 
+
 //Screen attributes
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
 extern const int SCREEN_BPP;
 
+// difficulty level
+extern int level;
+
 //The frames per second
 extern const int FRAMES_PER_SECOND;
 
 //The dimenstions of the stick figure
-extern const int FOO_WIDTH;
-extern const int FOO_HEIGHT;
+extern const int AIRMAN_WIDTH;
+extern const int AIRMAN_HEIGHT;
 extern const int JUMP_WIDTH;
 extern const int JUMP_HEIGHT;
 extern int DEFAULT_Y;
 extern int jump;
 
 //The direction status of the stick figure
-extern const int FOO_RIGHT;
-extern const int FOO_LEFT;
-extern const int FOO_JUMP_RIGHT;
-extern const int FOO_JUMP_LEFT;
+extern const int AIRMAN_RIGHT;
+extern const int AIRMAN_LEFT;
+extern const int AIRMAN_JUMP_RIGHT;
+extern const int AIRMAN_JUMP_LEFT;
 
 // dimensions of our obstacle
 extern int OBST_WIDTH;
@@ -37,11 +41,18 @@ extern int OBST_XCOORD;
 extern int OBST_YCOORD;
 
 //The surfaces
-extern SDL_Surface *foo;
+extern SDL_Surface *airman;
 extern SDL_Surface *background;
 extern SDL_Surface *obstacle;
 extern SDL_Surface *screen;
 extern SDL_Surface *enemy;
+extern SDL_Surface *boss;
+extern SDL_Surface *bullet1;
+extern SDL_Surface *bullet2;
+extern SDL_Surface *bullet3;
+extern SDL_Surface *gameover;
+extern SDL_Surface *start;
+extern SDL_Surface *winner;
 
 //The music
 extern Mix_Music *music;
@@ -54,19 +65,37 @@ extern SDL_Rect clipsRight[];
 extern SDL_Rect clipsLeft[];
 extern SDL_Rect clipsUpRight[];
 extern SDL_Rect clipsUpLeft[];
-extern SDL_Rect fooRect;
+extern SDL_Rect airmanRect;
 extern SDL_Rect enemyRect;
 extern SDL_Rect obstacleRect;
+extern SDL_Rect obstacleRect1;
+extern SDL_Rect obstacleRect2;
+extern SDL_Rect obstacleRect3;
+extern SDL_Rect obstacleRect4;
+extern SDL_Rect obstacleRect5;
+extern SDL_Rect obstacleRect6;
+extern SDL_Rect obstacleRect7;
+extern SDL_Rect obstacleRect8;
+extern SDL_Rect obstacleRect9;
+extern SDL_Rect obstacleRect10;
+extern SDL_Rect obstacleRect11;
+extern SDL_Rect obstacleRect12;
+extern SDL_Rect obstacleRect13;
+extern SDL_Rect obstacleRect14;
+
+
 
 //The stick figure
-class Foo
+class Airman
 {
     private:
     //The offset
     int offSet;
+    int yoff;
 
     //Its rate of movement
     int velocity;
+    int yvel;
 
     //Its current frame
     int frame;
@@ -76,7 +105,7 @@ class Foo
 
     public:
     //Initializes the variables
-    Foo();
+    Airman();
 
     //Handles input
     void handle_events();
@@ -86,6 +115,9 @@ class Foo
 
     //Shows the stick figure
     void show();
+
+    int getoffset(){ return offSet;};
+    int getyoff(){ return yoff;};
 };
 
 //The timer
@@ -173,8 +205,3 @@ inline bool collision( SDL_Rect A, SDL_Rect B ){
     
 }
 #endif
-
-
-
-
-
